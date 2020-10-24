7ba6dfde6c6ebba5a6b0d7958e18b2714818f570
Yaekith <yaekith@gmail.com>
2020-10-25 06:52:52 +0800

    Update Program.cs

--- CordwareNoGUIInjector/Program.cs
+++ CordwareNoGUIInjector/Program.cs
@@ -37,14 +37,18 @@ namespace CordwareNoGUIInjector
                         ConsoleUtils.LogError("Already Injected.");
                     else
                     {
-                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\index.js", new HttpClient().GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/index.txt").Result.Content.ReadAsStringAsync().Result);
+                        HttpClient httpclient = new HttpClient();
+                        httpclient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
+
+                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\index.js", httpclient.GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/index.txt").Result.Content.ReadAsStringAsync().Result);
                         Directory.CreateDirectory($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware");
-                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\payload.js", new HttpClient().GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/payload.js").Result.Content.ReadAsStringAsync().Result);
-                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\client.js", new HttpClient().GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/client.js").Result.Content.ReadAsStringAsync().Result);
+                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\payload.js", httpclient.GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/payload.js").Result.Content.ReadAsStringAsync().Result);
+                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\client.js", httpclient.GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/client.js").Result.Content.ReadAsStringAsync().Result);
                         Directory.CreateDirectory($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\API");
-                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\API\\API.js", new HttpClient().GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/API/API.js").Result.Content.ReadAsStringAsync().Result);
+                        File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\API\\API.js", httpclient.GetAsync("https://raw.githubusercontent.com/Yaekith/Cordware/main/API/API.js").Result.Content.ReadAsStringAsync().Result);
                         Directory.CreateDirectory($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\Plugins");
-                        var MainBranch = JsonConvert.DeserializeObject<TreeBranchGather>(new HttpClient().GetAsync("https://api.github.com/repos/Yaekith/Cordware/git/trees/main?recursive=1").Result.Content.ReadAsStringAsync().Result);
+                        var branch = httpclient.GetAsync("https://api.github.com/repos/Yaekith/Cordware/git/trees/main?recursive=1").Result.Content.ReadAsStringAsync().Result;
+                        var MainBranch = JsonConvert.DeserializeObject<TreeBranchGather>(branch);
                         int pluginCount = 0;
 
                         foreach(var path in MainBranch.tree)
@@ -52,7 +56,7 @@ namespace CordwareNoGUIInjector
                             if (path.path.Contains("Plugins") && path.path != "Plugins")
                             {
                                 pluginCount++;
-                                File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\Plugins\\{path.path.Split('/')[1]}", new HttpClient().GetAsync($"https://raw.githubusercontent.com/Yaekith/Cordware/main/{path.path}").Result.Content.ReadAsStringAsync().Result);
+                                File.WriteAllText($"{versionfolderpath}\\modules\\discord_desktop_core\\Cordware\\Plugins\\{path.path.Split('/')[1]}", httpclient.GetAsync($"https://raw.githubusercontent.com/Yaekith/Cordware/main/{path.path}").Result.Content.ReadAsStringAsync().Result);
                             }
                         }
 
